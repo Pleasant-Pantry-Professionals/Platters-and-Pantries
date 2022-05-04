8f9d828af293b643edd64731c00f11a42e9f15dc
Theo Ward <theoward9@gmail.com>
2022-05-03 19:58:13 -0500

    created relationships between models

--- models/index.js
+++ models/index.js
@@ -10,6 +10,47 @@ const RecipeIngredient = require('./RecipeIngredients');
 //comments on recipes?
 
 //set up relationships between models
+//Pantry belongs to user
+Pantry.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//ShoppingList belongs to user
+ShoppingList.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//User has one Pantry and one Shopping List
+User.hasOne(Pantry, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+User.hasOne(ShoppingList, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+//lists have many ingredients
+Pantry.hasMany(Ingredient, {
+    foreignKey: 'ingredient_id',
+});
+ShoppingList.hasMany(Ingredient, {
+    foreignKey: 'ingredient_id',
+});
+RecipeIngredient.hasMany(Ingredient, {
+    foreignKey: 'ingredient_id,'
+});
+
+//ingredients belong many places
+Ingredient.belongsTo(Pantry, {
+    foreignKey: 'pantry_id',
+});
+Ingredient.belongsTo(ShoppingList, {
+    foreignKey: 'shoppinglist_id',
+});
+Ingredient.belongsTo(RecipeIngredient, {
+    foreignKey: 'recipeingredient_id',
+});
 
 module.exports = {
     User,
